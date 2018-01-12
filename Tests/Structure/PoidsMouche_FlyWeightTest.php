0fdda1c81c038ba7e82b61944ee24106adb109a4
wassim hazime <wassim.hazime@gmail.com>
2018-01-12 01:48:01 +0000

    test FlyWeight

--- Tests/Structure/PoidsMouche_FlyWeightTest.php
+++ Tests/Structure/PoidsMouche_FlyWeightTest.php
@@ -1,9 +1,22 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ En externalisant les attributs intrinsèques des objets (style de trait), on
+peut avoir en mémoire une seule instance correspondant à un groupe
+de valeurs (simple-continu-sans ombre, double-pointillé-ombre).
+Chaque objet avec des attributs extrinsèques (trait avec les
+coordonnées) possède une référence vers une instance d'attributs
+intrinsèques (style de trait).
+On obtient deux types de poids-mouche : les poids-mouche partagés
+(style de trait) et les poids mouche non partagés (le trait avec ses
+coordonnées).
+La partie cliente demande le poids-mouche qui l'intéresse à la fabrique
+de poids-mouche. S'il s'agit d'un poids mouche non partagé, la fabrique
+le créera et le retournera.
+S'il s'agit d'un poids-mouche partagé, la fabrique vérifiera si une
+instance existe. Si une instance existe, la fabrique la retourne, sinon la
+fabrique la crée et la retourne.
+
  */
 
 /**
