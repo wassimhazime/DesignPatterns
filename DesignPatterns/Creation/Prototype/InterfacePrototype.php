a13f413b165292f783cecbd9f9b1bcb8bc85a1da
wassim hazime <wassim.hazime@gmail.com>
2018-01-14 17:41:14 +0000

    PrototypeTest

---
+++ DesignPatterns/Creation/Prototype/InterfacePrototype.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace DesignPatterns\Creation\Prototype;
+
+/**
+ *
+ * @author wassime
+ */
+interface InterfacePrototype {
+    function copier() ;
+}
--- Tests/Creation/PrototypeTest.php
+++ Tests/Creation/PrototypeTest.php
@@ -1,9 +1,18 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *L’objectif du pattern PROTOTYPE est de fournir de nouveaux
+objets par la copie d’un exemple plutôt que de produire
+de nouvelles instances non initialisées d’une classe.
+Raisons d’utilisation
+◦ Le système doit créer de nouvelles instances, mais il ignore de
+quelle classe. Il dispose cependant d'instances de la classe
+désirée.
+◦ La duplication peut être également intéressante pour les
+performances (la duplication est plus rapide que l'instanciation).
+Résultat :
+◦ Le Design Pattern permet d'isoler l'appartenance à une classe.
+
  */
 
 /**
@@ -14,7 +23,7 @@
 class PrototypeTest extends \PHPUnit\Framework\TestCase{
     public function testclone() {
         $classeTs1=new DesignPatterns\Creation\Prototype\Classe("TS1", "wassim", 15);
-        $eleve1=new DesignPatterns\Creation\Prototype\PrototypeApp($classeTs1, "achraf", "hazime", 28, "casa");
+        $eleve1=new DesignPatterns\Creation\Prototype\Eleve($classeTs1, "achraf", "hazime", 28, "casa");
        
         $eleve2= clone $eleve1;
         $eleve3=  $eleve1->copier();
